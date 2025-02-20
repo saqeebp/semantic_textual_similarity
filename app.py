165416de817beca29681cfb540f246973c63954f
saqeebp <159631694+saqeebp@users.noreply.github.com>
2025-02-20 21:38:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,4 +15,5 @@ def handle_request():
         return jsonify({"error": str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    port = int(os.environ.get('PORT', 8000))  # Get PORT from environment
+    app.run(host='0.0.0.0', port=port)        # Don't hardcode port
